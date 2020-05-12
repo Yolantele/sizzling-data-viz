01a5770f4e70ea0aecc5d34368d9c4e3979e3dbe
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2020-05-12 21:10:15 +0100

    loads data files

---
+++ src/data/building_data.js
@@ -0,0 +1,122 @@
+export const buildingData = [
+  {
+    id: 9539,
+    name: 'Aberdeen'
+  },
+  {
+    id: 9554,
+    name: 'Ashton (6436)'
+  },
+  {
+    id: 9532,
+    name: 'Bournemouth (6428)'
+  },
+  {
+    id: 9511,
+    name: 'Bury (6423)'
+  },
+  {
+    id: 9577,
+    name: 'Cardiff (6420)'
+  },
+  {
+    id: 9555,
+    name: 'Cheadle (6417)'
+  },
+  {
+    id: 9533,
+    name: 'Coventry (6421)'
+  },
+  {
+    id: 9512,
+    name: 'Dudley (6422)'
+  },
+  {
+    id: 9518,
+    name: 'Edinburgh'
+  },
+  {
+    id: 9578,
+    name: 'Elstree (6437)'
+  },
+  {
+    id: 9556,
+    name: 'Farnborough (6434)'
+  },
+  {
+    id: 9510,
+    name: 'Glasgow'
+  },
+  {
+    id: 9534,
+    name: 'Herons Reach (0540)'
+  },
+  {
+    id: 9513,
+    name: 'Hull (6429)'
+  },
+  {
+    id: 9579,
+    name: 'Hyde (6416)'
+  },
+  {
+    id: 9557,
+    name: 'Leeds (6419)'
+  },
+  {
+    id: 9535,
+    name: 'Liverpool Whiston (6476)'
+  },
+  {
+    id: 9514,
+    name: 'Maidstone (6427)'
+  },
+  {
+    id: 9580,
+    name: 'Newcastle (6424)'
+  },
+  {
+    id: 9558,
+    name: 'Nottingham (6418)'
+  },
+  {
+    id: 9536,
+    name: 'Solihull (6435)'
+  },
+  {
+    id: 9515,
+    name: 'South Leeds (6432)'
+  },
+  {
+    id: 9581,
+    name: 'St Davids (0547)'
+  },
+  {
+    id: 9559,
+    name: 'Swansea (6431)'
+  },
+  {
+    id: 9537,
+    name: 'Swindon (0538)'
+  },
+  {
+    id: 9516,
+    name: 'Walsall (6426)'
+  },
+  {
+    id: 9582,
+    name: 'Warrington (6414)'
+  },
+  {
+    id: 9560,
+    name: 'Wirral (6413)'
+  },
+  {
+    id: 40615,
+    name: 'Portsmouth'
+  },
+  {
+    id: 91637,
+    name: 'Bristol'
+  }
+]
---
+++ src/data/meter_data.js
@@ -0,0 +1,704 @@
+export const meterData = [
+  {
+    building_id: 9539,
+    id: 196327,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9539,
+    id: 8718,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9539,
+    id: 12779,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9554,
+    id: 12849,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9554,
+    id: 8752,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9554,
+    id: 12847,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9532,
+    id: 12795,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9532,
+    id: 12797,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9532,
+    id: 8726,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9511,
+    id: 8742,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9511,
+    id: 8741,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9511,
+    id: 12827,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9577,
+    id: 8725,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9577,
+    id: 12793,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9577,
+    id: 8724,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9555,
+    id: 8761,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9555,
+    id: 12864,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9555,
+    id: 8760,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9533,
+    id: 8730,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9533,
+    id: 8731,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9533,
+    id: 12805,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9533,
+    id: 12803,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9512,
+    id: 12851,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9512,
+    id: 12853,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9512,
+    id: 8754,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9512,
+    id: 8753,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9518,
+    id: 196329,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9518,
+    id: 8719,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9518,
+    id: 12781,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9578,
+    id: 12831,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9578,
+    id: 8743,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9578,
+    id: 12829,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9556,
+    id: 196326,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9556,
+    id: 8732,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9556,
+    id: 12807,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9510,
+    id: 196328,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9510,
+    id: 53364,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9510,
+    id: 12570,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9534,
+    id: 12811,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9534,
+    id: 8733,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9534,
+    id: 12809,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9513,
+    id: 12825,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9513,
+    id: 8740,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9513,
+    id: 12823,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9513,
+    id: 8739,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9579,
+    id: 8763,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9579,
+    id: 12870,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9579,
+    id: 8762,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9579,
+    id: 12868,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9579,
+    id: 12866,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9557,
+    id: 12839,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9557,
+    id: 8749,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9557,
+    id: 12841,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9557,
+    id: 8748,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9557,
+    id: 8747,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9557,
+    id: 12837,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9557,
+    id: 8746,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9557,
+    id: 12835,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9535,
+    id: 8745,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9535,
+    id: 12833,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9535,
+    id: 8744,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9514,
+    id: 8722,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9514,
+    id: 12787,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9514,
+    id: 8721,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9514,
+    id: 12785,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9514,
+    id: 8720,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9514,
+    id: 12783,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9580,
+    id: 12860,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9580,
+    id: 12862,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9580,
+    id: 8759,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9580,
+    id: 8758,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9558,
+    id: 12858,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9558,
+    id: 12856,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9558,
+    id: 8757,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9536,
+    id: 12791,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9536,
+    id: 8723,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9536,
+    id: 12789,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9515,
+    id: 8729,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9515,
+    id: 12801,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9515,
+    id: 8728,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9515,
+    id: 12799,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9515,
+    id: 8727,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9581,
+    id: 12821,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9581,
+    id: 8738,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9581,
+    id: 12819,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9581,
+    id: 8737,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9559,
+    id: 8766,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9559,
+    id: 12878,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9559,
+    id: 8765,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9537,
+    id: 8750,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9537,
+    id: 12843,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9537,
+    id: 8751,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9537,
+    id: 12845,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9516,
+    id: 12817,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9516,
+    id: 8736,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9516,
+    id: 12815,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9516,
+    id: 8735,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9516,
+    id: 12813,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9516,
+    id: 8734,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9582,
+    id: 12872,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 9582,
+    id: 8764,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9582,
+    id: 12874,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9582,
+    id: 14933,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9582,
+    id: 14939,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9582,
+    id: 14937,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9582,
+    id: 12876,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9560,
+    id: 8756,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9560,
+    id: 14935,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9560,
+    id: 14943,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9560,
+    id: 14941,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 9560,
+    id: 8755,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 40615,
+    id: 196330,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 40615,
+    id: 83336,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 40615,
+    id: 83335,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  },
+  {
+    building_id: 91637,
+    id: 196331,
+    fuel: 'Water',
+    unit: 'm3'
+  },
+  {
+    building_id: 91637,
+    id: 195635,
+    fuel: 'Natural Gas',
+    unit: 'kWh'
+  },
+  {
+    building_id: 91637,
+    id: 195634,
+    fuel: 'Electricity',
+    unit: 'kWh'
+  }
+]
