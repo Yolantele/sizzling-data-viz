ab796d693653438a6c4786463c3fe4a9cc5c1af7
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2020-05-12 18:15:47 +0100

    sets the basic carcass for React App

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import testData from './data/testData'
+import React, { useState } from 'react'
+import { render } from 'react-dom'
+
+const App = ({}) => {
+  return <div>Hello</div>
+}
+
+export default CircularSankey
+render(<App />, document.getElementById('root'))
