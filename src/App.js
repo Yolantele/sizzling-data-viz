9a5b4744155823ed24c1b91353b85163c584128c
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2020-05-13 18:41:04 +0100

    uncomments controls panel for devexpress re-rendering - call on non existen val within library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { SteamGraph } from './comps'
+import React, { useState } from 'react'
+import { SteamGraph, ErrorBoundary, ControlsPanel } from './comps'
 import style from './style'
 
 const App = () => {
+  const [range, setRange] = useState({ start: '2018-12-01 07:00', end: '2018-12-01 13:00' })
+  const [number, setNumber] = useState(6)
   return (
-    <>
+    <ErrorBoundary>
       <header style={style.head} />
-      <div style={style.cont}>
-        <SteamGraph />
-      </div>
+      <main style={style.cont}>
+        <SteamGraph
+          start={range.start}
+          end={range.end}
+          numberOfBuildings={number}
+          style={{ margin: 30 }}
+        />
+        {/* <ControlsPanel {...{ number, setNumber }} /> */}
+      </main>
       <footer style={style.foot} />
-    </>
+    </ErrorBoundary>
   )
 }
 
