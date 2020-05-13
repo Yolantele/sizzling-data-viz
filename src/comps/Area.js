ea5b21c88e62a3cb50d42bef268b84ee076a34d1
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2020-05-13 15:31:07 +0100

    refactors

--- src/comps/Area.js
+++ src/comps/Area.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { AreaSeries } from '@devexpress/dx-react-chart-material-ui'
 import { curveCatmullRom, area } from 'd3-shape'
 
--- src/comps/TitleText.js
+++ src/comps/TitleText.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Title } from '@devexpress/dx-react-chart-material-ui'
 
 const TitleText = (props) => <Title.Text {...props} style={{ marginRight: '120px' }} />
